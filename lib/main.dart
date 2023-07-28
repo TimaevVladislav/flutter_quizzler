8cbb444145e5e24b14b81ac22f3900d84c833237
timaevV <timaevvladislav@gmail.com>
2023-07-29 02:43:24 +0600

    add:next void

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,17 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> icons = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  int questionId = 0;
+
+  void nextQuestion(icon, color) {
+    questionId++;
+    icons.add(Icon(Icons.check, color: Colors.green));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
               padding: EdgeInsets.all(10.0),
               child: Center(
                 child: Text(
-                  'This is where the question text will go.',
+                  questions[questionId],
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 25.0, color: Colors.white),
                 ),
@@ -60,7 +71,7 @@ class _QuizPageState extends State<QuizPage> {
                     )),
                 onPressed: () {
                   setState(() {
-                    icons.add(Icon(Icons.check, color: Colors.green));
+                    nextQuestion(Icons.check, Colors.green);
                   });
                 },
               ),
@@ -82,7 +93,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
                 onPressed: () {
                   setState(() {
-                    icons.add(Icon(Icons.close, color: Colors.red));
+                    nextQuestion(Icons.close, Colors.red);
                   });
                 },
               ),
